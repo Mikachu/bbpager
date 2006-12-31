1e8743506c3cff9710b6be79f8e0595d86981913
jkennis <jkennis>
2006-12-31 17:43:24 +0000

    Fixed indentation

--- src/Baseresource.cxx
+++ src/Baseresource.cxx
@@ -25,56 +25,57 @@
 #include "Menu.hh"
 
 BaseResource::BaseResource(bt::Application &_app, unsigned int _screen, const std::string &blackbox_rc_filename, const std::string &filename) : 
-	app(_app), display(_app.display())
+    app(_app), display(_app.display())
 {
-	const bt::ScreenInfo& screeninfo = _app.display().screenInfo(_screen);
-	screen = _screen;
+    const bt::ScreenInfo& screeninfo = _app.display().screenInfo(_screen);
+    screen = _screen;
 
-	// get blackbox configuration file
+    // get blackbox configuration file
     if (!blackbox_rc_filename.empty())
     {
         bt_resource.load(blackbox_rc_filename);
     }
     else
     {
-    	bt_resource.load(std::string("~/.blackboxrc"));
+        bt_resource.load(std::string("~/.blackboxrc"));
     }
     
-	if (bt_resource.valid()) 
+    if (bt_resource.valid()) 
     {
-		const std::string blackbox_stylefile = bt_resource.read("session.styleFile", "Session.StyleFile", "");
+        const std::string blackbox_stylefile = bt_resource.read("session.styleFile", "Session.StyleFile", "");
 
-		colors_per_channel = readInt("session.colors_per_channel", "Session.Colors_per_channel", 4);
-		if (colors_per_channel < 2) colors_per_channel = 2;
-		if (colors_per_channel > 6) colors_per_channel = 6;
+        colors_per_channel = readInt("session.colors_per_channel", "Session.Colors_per_channel", 4);
+        if (colors_per_channel < 2) colors_per_channel = 2;
+        if (colors_per_channel > 6) colors_per_channel = 6;
 
-		image_dither = readBool("session.imageDither", "Session.ImageDither", true);
+        image_dither = readBool("session.imageDither", "Session.ImageDither", true);
 
-		if (image_dither &&
-		    screeninfo.visual()->c_class == TrueColor &&
-	            screeninfo.depth() >= 24)
-			image_dither = false;
+        if (image_dither &&
+            screeninfo.visual()->c_class == TrueColor &&
+                screeninfo.depth() >= 24)
+            image_dither = false;
 
-		if (!blackbox_stylefile.empty()) {
-			bt_resource.load(blackbox_stylefile);
-		}
+        if (!blackbox_stylefile.empty()) 
+        {
+            bt_resource.load(blackbox_stylefile);
+        }
 #if 0
-  	    bool use_default = true;
-		if (!filename.empty()) {
-			if (!bt_resource.merge(filename)) {
-				fprintf(stderr, "warning: Cannot open resource file %s, using default\n", filename.c_str());
-			} else {
-				use_default = false;
-			}
-		}
-		
-		if (use_default) {
-			if (!bt_resource.merge(std::string(BBTOOL_LOCAL))) {
-				if (!bt_resource.merge(std::string(BBTOOL_GLOBAL))) {
-					fprintf(stderr, "Warning: Cannot open resource files, using internal defaults\n");
-				}
-			}
-		}
+        bool use_default = true;
+        if (!filename.empty()) {
+            if (!bt_resource.merge(filename)) {
+                fprintf(stderr, "warning: Cannot open resource file %s, using default\n", filename.c_str());
+            } else {
+                use_default = false;
+            }
+        }
+        
+        if (use_default) {
+            if (!bt_resource.merge(std::string(BBTOOL_LOCAL))) {
+                if (!bt_resource.merge(std::string(BBTOOL_GLOBAL))) {
+                    fprintf(stderr, "Warning: Cannot open resource files, using internal defaults\n");
+                }
+            }
+        }
 #endif
     }
     if (!filename.empty()) {
@@ -92,90 +93,94 @@ BaseResource::~BaseResource()
 
 std::string BaseResource::readString(const std::string &rname, const std::string &rclass, const std::string &default_val)
 {
- 	return(bt_resource.read(rname, rclass, default_val));
+    return(bt_resource.read(rname, rclass, default_val));
 }
 
 std::string BaseResource::readString(const std::string &rname, const std::string &rclass,
-				     const std::string &alt_rname, const std::string &alt_rclass,
-				     const std::string &default_val)
+                     const std::string &alt_rname, const std::string &alt_rclass,
+                     const std::string &default_val)
 {
-	std::string tmp = bt_resource.read(rname, rclass, "");
-	if (tmp.empty())
-		return(bt_resource.read(alt_rname, alt_rclass, default_val));
-	return(tmp);
+    std::string tmp = bt_resource.read(rname, rclass, "");
+    if (tmp.empty())
+        return(bt_resource.read(alt_rname, alt_rclass, default_val));
+    return(tmp);
 }
 
 int BaseResource::readInt(const std::string &rname, const std::string &rclass, int default_val)
 {
-  	int ret_val;
-	std::string tmp = bt_resource.read(rname, rclass, "");
-	if ((tmp.empty()) || (sscanf(tmp.c_str(), "%d", &ret_val) != 1))
-		return(default_val);
-	return(ret_val);
+    int ret_val;
+    std::string tmp = bt_resource.read(rname, rclass, "0");
+    if ((tmp.empty()) || (sscanf(tmp.c_str(), "%d", &ret_val) != 1))
+    {
+        return(default_val);
+    }
+    return(ret_val);
 }
 
 unsigned int BaseResource::readUInt(const std::string &rname, const std::string &rclass, unsigned int default_val)
 {
-  	int ret_val;
-	std::string tmp = bt_resource.read(rname, rclass, "");
-	if ((tmp.empty()) || (sscanf(tmp.c_str(), "%u", &ret_val) != 1))
-		return(default_val);
-	return(ret_val);
+    int ret_val;
+    std::string tmp = bt_resource.read(rname, rclass, "");
+    if ((tmp.empty()) || (sscanf(tmp.c_str(), "%u", &ret_val) != 1))
+    {
+        return(default_val);
+    }
+    return(ret_val);
 }
 
 unsigned int BaseResource::readUInt(const std::string &rname, const std::string &rclass, 
-				    const std::string &alt_rname, const std::string &alt_rclass, 
-				    unsigned int default_val)
+                    const std::string &alt_rname, const std::string &alt_rclass, 
+                    unsigned int default_val)
 {
-  	int ret_val;
-	std::string tmp = bt_resource.read(rname, rclass, "");
-	if (tmp.empty()) {
-		std::string tmp = bt_resource.read(alt_rname, alt_rclass, "");
-		if ((tmp.empty()) || (sscanf(tmp.c_str(), "%u", &ret_val) != 1))
-			return(default_val);
-	}
-	return(ret_val);
+    int ret_val;
+    std::string tmp = bt_resource.read(rname, rclass, "");
+    if (tmp.empty()) {
+        std::string tmp = bt_resource.read(alt_rname, alt_rclass, "");
+        if ((tmp.empty()) || (sscanf(tmp.c_str(), "%u", &ret_val) != 1))
+            return(default_val);
+    }
+    return(ret_val);
 }
 
 bool BaseResource::readBool(const std::string &rname, const std::string &rclass, bool default_val)
 {
-	std::string tmp = bt_resource.read(rname, rclass, "");
-	if (!strcasecmp(tmp.c_str(),"true")) 
-		return(true); 
-	if (!strcasecmp(tmp.c_str(),"false"))
-		return(false);
+    std::string tmp = bt_resource.read(rname, rclass, "");
+    if (!strcasecmp(tmp.c_str(),"true")) 
+        return(true); 
+    if (!strcasecmp(tmp.c_str(),"false"))
+        return(false);
 
-	return(default_val);
+    return(default_val);
 }
 
 bt::Color BaseResource::readColor(const std::string &rname,const std::string &rclass,
-	                       	  const std::string &default_color)
+                              const std::string &default_color)
 {
-	std::string rcolor = bt_resource.read(rname, rclass, default_color);
-	bt::Color color;
-	color = bt::Color::namedColor(display, screen, rcolor);
-	return(color);
+    std::string rcolor = bt_resource.read(rname, rclass, default_color);
+    bt::Color color;
+    color = bt::Color::namedColor(display, screen, rcolor);
+    return(color);
 }
 
 
 bt::Color BaseResource::readColor(const std::string &rname,const std::string &rclass,
-				  const std::string &alt_rname, const std::string &alt_rclass,
-	                       	  const std::string &default_color)
+                  const std::string &alt_rname, const std::string &alt_rclass,
+                              const std::string &default_color)
 {
-	std::string rcolor = bt_resource.read(rname, rclass, "");
-	if (rcolor.empty()) {
-		rcolor = bt_resource.read(alt_rname, alt_rclass, default_color);
-	}
-	bt::Color color;
-	color = bt::Color::namedColor(display, screen, rcolor);
-	return(color);
+    std::string rcolor = bt_resource.read(rname, rclass, "");
+    if (rcolor.empty()) {
+        rcolor = bt_resource.read(alt_rname, alt_rclass, default_color);
+    }
+    bt::Color color;
+    color = bt::Color::namedColor(display, screen, rcolor);
+    return(color);
 }
 
 bt::Texture BaseResource::readTexture(const std::string &rname, 
-				      const std::string &rclass,
-			       	  const std::string &default_texture,
-			 	      const std::string &default_color,
-				      const std::string &default_colorTo)
+                      const std::string &rclass,
+                      const std::string &default_texture,
+                      const std::string &default_color,
+                      const std::string &default_colorTo)
 {
   bt::Texture dtexture;
   dtexture.setDescription(default_texture);
@@ -187,12 +192,12 @@ bt::Texture BaseResource::readTexture(const std::string &rname,
 
 
 bt::Texture BaseResource::readTexture(const std::string &rname, 
-				      const std::string &rclass,
-				      const std::string &alt_rname,
-				      const std::string &alt_rclass,
-			       	  const std::string &default_texture,
-			 	      const std::string &default_color,
-				      const std::string &default_colorTo)
+                      const std::string &rclass,
+                      const std::string &alt_rname,
+                      const std::string &alt_rclass,
+                      const std::string &default_texture,
+                      const std::string &default_color,
+                      const std::string &default_colorTo)
 
 {
   bt::Texture dtexture;
@@ -217,30 +222,30 @@ bt::Texture BaseResource::readTexture(const std::string &rname,
 }
 
 bt::Font BaseResource::readFont(const std::string &rname, 
-				const std::string &rclass,
-				const std::string &alt_rname,
-				const std::string &alt_rclass)
+                const std::string &rclass,
+                const std::string &alt_rname,
+                const std::string &alt_rclass)
 {
-	std::string font_name = bt_resource.read(rname, rclass, "");
-	if (font_name.empty()) {
-		font_name = bt_resource.read(alt_rname, alt_rclass, "");
-	}
-	bt::Font font(font_name);
-	return(font);
+    std::string font_name = bt_resource.read(rname, rclass, "");
+    if (font_name.empty()) {
+        font_name = bt_resource.read(alt_rname, alt_rclass, "");
+    }
+    bt::Font font(font_name);
+    return(font);
 }
 
 void BaseResource::loadMenuStyle(void)
 {
-	bt::MenuStyle::get(app, screen)->load(bt_resource);
+    bt::MenuStyle::get(app, screen)->load(bt_resource);
 }
 
 
 std::string BaseResource::getColorName(const bt::Color &color)
 {
-	char color_name[8];
+    char color_name[8];
 
-	snprintf(color_name, 8, "#%02x%02x%02X", color.red(), color.green(), color.blue());
-	return std::string(color_name);
+    snprintf(color_name, 8, "#%02x%02x%02X", color.red(), color.green(), color.blue());
+    return std::string(color_name);
 }
 
 
--- src/resource.cxx
+++ src/resource.cxx
@@ -23,14 +23,14 @@
 #include "blackboxstyle.h"
 
 Resource::Resource(ToolWindow *toolwindow, const std::string &blackbox_rc_file, const std::string &rc_file): 
-		BaseResource(*toolwindow, toolwindow->getCurrentScreen(), blackbox_rc_file, rc_file), bbtool(toolwindow)
+        BaseResource(*toolwindow, toolwindow->getCurrentScreen(), blackbox_rc_file, rc_file), bbtool(toolwindow)
 {
-	load();
+    load();
 }
 
 Resource::~Resource() 
 {
-	clean();
+    clean();
 }
 
 void Resource::clean() 
@@ -41,90 +41,90 @@ void Resource::clean()
 
 void Resource::load(void) 
 {
-	unsigned int button = 0;
-	WHICH_BUTTON move_default = LEFT_BUTTON;
+    unsigned int button = 0;
+    WHICH_BUTTON move_default = LEFT_BUTTON;
 
-	button = readUInt("bbpager.desktopChangeButton", "Bbpager.Desktopchangebutton", MIDDLE_BUTTON);
-	if (button > 5 || button < 1) {
-		button = MIDDLE_BUTTON;
-	}
-	desktop_change_button = static_cast<WHICH_BUTTON>(button);
-	if (desktop_change_button == LEFT_BUTTON) {
-		move_default = MIDDLE_BUTTON;
-	}
+    button = readUInt("bbpager.desktopChangeButton", "Bbpager.Desktopchangebutton", MIDDLE_BUTTON);
+    if (button > 5 || button < 1) {
+        button = MIDDLE_BUTTON;
+    }
+    desktop_change_button = static_cast<WHICH_BUTTON>(button);
+    if (desktop_change_button == LEFT_BUTTON) {
+        move_default = MIDDLE_BUTTON;
+    }
 
-	button = readUInt("bbpager.windowMoveButton", "Bbpager.WindowMovebutton", move_default);
-	if (button > 5 || button < 1) {
-		button = move_default;
-	}
-	window_move_button = static_cast<WHICH_BUTTON>(button);
+    button = readUInt("bbpager.windowMoveButton", "Bbpager.WindowMovebutton", move_default);
+    if (button > 5 || button < 1) {
+        button = move_default;
+    }
+    window_move_button = static_cast<WHICH_BUTTON>(button);
 
-	button = readUInt("bbpager.windowFocusButton", "Bbpager.WindowFocusbutton", INVALID_BUTTON);
-	if (button > 5 || button < 1) {
-		button = INVALID_BUTTON;
-	}
-	window_focus_button = static_cast<WHICH_BUTTON>(button);
+    button = readUInt("bbpager.windowFocusButton", "Bbpager.WindowFocusbutton", INVALID_BUTTON);
+    if (button > 5 || button < 1) {
+        button = INVALID_BUTTON;
+    }
+    window_focus_button = static_cast<WHICH_BUTTON>(button);
 
-	button = readUInt("bbpager.windowRaiseButton", "Bbpager.WindowRaisebutton", INVALID_BUTTON);
-	if (button > 5 || button < 1) {
-		button = INVALID_BUTTON;
-	}
-	window_raise_button = static_cast<WHICH_BUTTON>(button);
+    button = readUInt("bbpager.windowRaiseButton", "Bbpager.WindowRaisebutton", INVALID_BUTTON);
+    if (button > 5 || button < 1) {
+        button = INVALID_BUTTON;
+    }
+    window_raise_button = static_cast<WHICH_BUTTON>(button);
 
-	Frame();
+    Frame();
  
-	SizeAndPosition();
+    SizeAndPosition();
 
-	PagerWin();
+    PagerWin();
 }
 
 
 void Resource::Frame() 
 {
-	frame.texture = readTexture("bbpager.frame","BbPager.Frame",BB_FRAME, BB_C_FRAME,
-				    "Raised Gradient Vertical", "slategrey","darkslategrey");
+    frame.texture = readTexture("bbpager.frame","BbPager.Frame",BB_FRAME, BB_C_FRAME,
+                    "Raised Gradient Vertical", "slategrey","darkslategrey");
 
-	frame.bevelWidth = readUInt("bbpager.bevelWidth","Bbpager.BevelWidth", 
+    frame.bevelWidth = readUInt("bbpager.bevelWidth","Bbpager.BevelWidth", 
                                 readUInt( "bbpager.margin","Bbpager.margin", 4));
-	if (frame.bevelWidth == 0)
-		frame.bevelWidth = 4;
+    if (frame.bevelWidth == 0)
+        frame.bevelWidth = 4;
 
-	frame.desktopMargin = readUInt("bbpager.desktop.bevelWidth","Bbpager.desktop.BevelWidth", 
+    frame.desktopMargin = readUInt("bbpager.desktop.bevelWidth","Bbpager.desktop.BevelWidth", 
                                 readUInt( "bbpager.desktop.margin","Bbpager.desktop.margin", frame.bevelWidth));
-	if (frame.desktopMargin == 0)
-		frame.desktopMargin = 4;
+    if (frame.desktopMargin == 0)
+        frame.desktopMargin = 4;
 }
 
 
 void Resource::SizeAndPosition() 
 {
-	unsigned int w, h;
+    unsigned int w, h;
 
-	if (!(bbtool->configuration().isWithdrawn())) 
-		bbtool->configuration().setWithdrawn(readBool("bbpager.withdrawn", "Bbpager.Withdrawn", false));
+    if (!(bbtool->configuration().isWithdrawn())) 
+        bbtool->configuration().setWithdrawn(readBool("bbpager.withdrawn", "Bbpager.Withdrawn", false));
 
-	if (!(bbtool->configuration().isShaped())) 
-		bbtool->configuration().setShaped(readBool("bbpager.shape", "Bbpager.Shape", false /*bbtool->configuration().isWithdrawn() */));
+    if (!(bbtool->configuration().isShaped())) 
+        bbtool->configuration().setShaped(readBool("bbpager.shape", "Bbpager.Shape", false /*bbtool->configuration().isWithdrawn() */));
 
-	if (bbtool->configuration().geometry().empty()) {
-		std::string positionstring = readString("bbpager.position","Bbpager.Position", "-0-0");
-		position.mask = XParseGeometry(positionstring.c_str(), &position.x, &position.y, &w, &h);
-		if (!(position.mask & XValue))
-			position.x = 0;
-		if (!(position.mask & YValue))
-			position.y = 0;
-	} else {
-    	position.mask = XParseGeometry(bbtool->configuration().geometry().c_str(), &position.x, &position.y, &w, &h);
-		if (!(position.mask & XValue))
-			position.x = 0;
-		if (!(position.mask & YValue))
-			position.y = 0;
+    if (bbtool->configuration().geometry().empty()) {
+        std::string positionstring = readString("bbpager.position","Bbpager.Position", "-0-0");
+        position.mask = XParseGeometry(positionstring.c_str(), &position.x, &position.y, &w, &h);
+        if (!(position.mask & XValue))
+            position.x = 0;
+        if (!(position.mask & YValue))
+            position.y = 0;
+    } else {
+        position.mask = XParseGeometry(bbtool->configuration().geometry().c_str(), &position.x, &position.y, &w, &h);
+        if (!(position.mask & XValue))
+            position.x = 0;
+        if (!(position.mask & YValue))
+            position.y = 0;
 
     }
 
     position.horizontal = false;
     position.vertical = false;
- 	std::string orientation = readString( "bbpager.desktop.orientation", "Bbpager.Desktop.Orientation", "horizontal");
+    std::string orientation = readString( "bbpager.desktop.orientation", "Bbpager.Desktop.Orientation", "horizontal");
 
     if (orientation == "vertical") {
        position.vertical = true;
@@ -133,53 +133,53 @@ void Resource::SizeAndPosition()
     }   
 
 
-  	columns = readUInt("bbpager.desktop.columns", "Bbpager.Desktop.Columns", 0xFFFF);
-	if (columns == 0) 
-	  columns = 0xFFFF;
+    columns = readUInt("bbpager.desktop.columns", "Bbpager.Desktop.Columns", 0xFFFF);
+    if (columns == 0) 
+      columns = 0xFFFF;
 
-	rows = readUInt("bbpager.desktop.rows","Bbpager.Desktop.Rows", 0XFFFF);
-	if (rows == 0) 
-		rows = 0xFFFF;
+    rows = readUInt("bbpager.desktop.rows","Bbpager.Desktop.Rows", 0XFFFF);
+    if (rows == 0) 
+        rows = 0xFFFF;
   
-	int default_width;
-	if (!bbtool->configuration().isWithdrawn())
-		default_width = 40;
-	else
-		default_width = 64;
+    int default_width;
+    if (!bbtool->configuration().isWithdrawn())
+        default_width = 40;
+    else
+        default_width = 64;
 
-	desktopSize.width = readUInt("bbpager.desktop.width", "Bbpager.Desktop.Width", default_width);
+    desktopSize.width = readUInt("bbpager.desktop.width", "Bbpager.Desktop.Width", default_width);
       
-	int default_height;
-	if (!bbtool->configuration().isWithdrawn())
-		default_height = 30;
-	else
-		default_height = 48;
+    int default_height;
+    if (!bbtool->configuration().isWithdrawn())
+        default_height = 30;
+    else
+        default_height = 48;
 
-	desktopSize.height = readUInt("bbpager.desktop.height", "Bbpager.Desktop.Height", default_height);
+    desktopSize.height = readUInt("bbpager.desktop.height", "Bbpager.Desktop.Height", default_height);
 }
 
 void Resource::PagerWin() 
 {
-	std::string focus_style;
+    std::string focus_style;
 
-	focus_style = readString( "bbpager.desktop.focusStyle", "Bbpager.Desktop.FocusStyle", "border");
+    focus_style = readString( "bbpager.desktop.focusStyle", "Bbpager.Desktop.FocusStyle", "border");
 
     if (strcasecmp("texture", focus_style.c_str()) == 0) {
-		desktop_focus_style = texture;
+        desktop_focus_style = texture;
     } else if (! strcasecmp("none", focus_style.c_str())) 
-		desktop_focus_style = none;
-	else 
-		desktop_focus_style = border;
+        desktop_focus_style = none;
+    else 
+        desktop_focus_style = border;
   
           desktopwin.texture = readTexture("bbpager.desktop", "Bbpager.Desktop",
                                            BB_LABEL,BB_C_LABEL,
                                            "Sunken Gradient Diagonal",
-					 "slategrey","darkslategrey");
+                     "slategrey","darkslategrey");
 
-	if (desktop_focus_style == texture) {
-		desktopwin.focusedTexture = readTexture("bbpager.desktop.focus", "Bbpager.Desktop.Focus",
-						       "Sunken Gradient Diagonal",
-						       "darkslategrey","slategrey");
+    if (desktop_focus_style == texture) {
+        desktopwin.focusedTexture = readTexture("bbpager.desktop.focus", "Bbpager.Desktop.Focus",
+                               "Sunken Gradient Diagonal",
+                               "darkslategrey","slategrey");
         }
 
         // Set the borders for active (focused) and inactive (unfocused) desktops.
@@ -211,6 +211,7 @@ void Resource::PagerWin()
           default_desktop_active_width = 0;
         }
 
+
         desktopwin.inactiveColor = readColor("bbpager.inactive.desktop.borderColor",
                                              "Bbpager.Inactive.desktop.BorderColor",
                                              "black");
@@ -232,28 +233,29 @@ void Resource::PagerWin()
                                            default_desktop_active_width);
         }
 
+        
 
-	std::string window_focus_style = readString("bbpager.window.focusStyle", "Bbpager.Window.FocusStyle", 
-						    "texture");
-	
-   	if (! strcasecmp("border", window_focus_style.c_str()))
-		pager_focus_style = border;
-	else if (! strcasecmp("none", window_focus_style.c_str())) 
-		pager_focus_style = none;
-	else 
-		pager_focus_style = texture;
+    std::string window_focus_style = readString("bbpager.window.focusStyle", "Bbpager.Window.FocusStyle", 
+                            "texture");
+    
+    if (! strcasecmp("border", window_focus_style.c_str()))
+        pager_focus_style = border;
+    else if (! strcasecmp("none", window_focus_style.c_str())) 
+        pager_focus_style = none;
+    else 
+        pager_focus_style = texture;
 
 
           pagerwin.texture = readTexture("bbpager.window", "Bbpager.Window",
                                          BB_WINDOW_UNFOCUS, BB_C_WINDOW_UNFOCUS,
                                          "Raised Gradient Diagonal",
-				       "rgb:c/9/6","rgb:8/6/4");
+                       "rgb:c/9/6","rgb:8/6/4");
 
-	if (pager_focus_style == texture) {
-		pagerwin.focusedTexture = readTexture("bbpager.window.focus","Bbpager.Window.Focus",
-						       BB_WINDOW_FOCUS,BB_C_WINDOW_FOCUS,
-		  				      "Raised Vertical Gradient",
-						      "rgb:c/9/6","rgb:8/6/4");
+    if (pager_focus_style == texture) {
+        pagerwin.focusedTexture = readTexture("bbpager.window.focus","Bbpager.Window.Focus",
+                               BB_WINDOW_FOCUS,BB_C_WINDOW_FOCUS,
+                              "Raised Vertical Gradient",
+                              "rgb:c/9/6","rgb:8/6/4");
         }
 
         // Set the borders for active (focused) and inactive (unfocused) windows.
