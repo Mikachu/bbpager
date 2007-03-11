14881a3d0c8d603d3b1271f02061dd8b1418639b
jkennis <jkennis>
2007-03-11 21:47:32 +0000

    Remove debugging messages

--- src/resource.cxx
+++ src/resource.cxx
@@ -147,8 +147,6 @@ void Resource::SizeAndPosition()
     else
         default_width = 64;
 
-
-    fprintf(stderr, "desktopsize\n");
     desktopSize.width = default_width;
     desktopSize.width = readUInt("bbpager.desktop.width", "Bbpager.Desktop.Width", default_width);
       
